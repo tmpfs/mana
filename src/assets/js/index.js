6f9cba316cafe36c4a6be7bda55ef1d2c7abb30d
muji <freeformsystems@gmail.com>
2017-10-07 22:59:05 +0800

    Disable selected state code.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,9 +4,25 @@
  *  Ubud mana villas
  */
 class Application {
-  constructor() {
-    console.log('Running project: mana')
+  start () {
+    // Handle selected state for room sub navigation
+    /*
+    window.addEventListener('hashchange', (e) => {
+      const hash = document.location.hash
+      const selection = document.querySelectorAll('.room-nav .selected')
+      for (let i = 0; i < selection.length; i++) {
+        if (selection[i].classList) {
+          selection[i].classList.remove('selected')
+        }
+      }
+      const el = document.querySelector(`a[href="${hash}"]`)
+      if (el && el.classList) {
+        el.classList.add('selected')
+      }
+    })
+    */
   }
 }
 
-new Application();
+const app = new Application()
+app.start()
