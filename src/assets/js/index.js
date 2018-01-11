48e2d73e44d6b7233912001c4f03b0c020000a47
muji <muji@tmpfs.org>
2018-01-11 17:42:46 +0800

    Experiment with stretching iframe height.
    Cannot due to cross origin policy.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -10,6 +10,15 @@ class Application {
   start () {
     this.scroller = new Scroll();
     this.slideshow = new SlideShow({ autoStart: true });
+
+    //const frames = Array.from(document.querySelectorAll('iframe'))
+      //console.log(frames)
+    //frames.forEach((frame) => {
+      //frame.onload = () => {
+        //frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
+        //console.log(frame.style.height)
+      //}
+    //})
   }
 }
 
