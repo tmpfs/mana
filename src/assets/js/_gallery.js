2fdb2427e090a8bc7029e3ad6a23227458749e38
muji <muji@tmpfs.org>
2018-01-12 11:16:50 +0800

    Configure gallery for boutique.

--- src/assets/js/_gallery.js
+++ src/assets/js/_gallery.js
@@ -1,96 +1,150 @@
 module.exports = {
   sari: [
     {
-      "pid": "01.joglo-front",
-      "src": "/assets/img/gallery/sari/01.joglo-front.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '01.joglo-front',
+      'src': '/assets/img/gallery/sari/01.joglo-front.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "02.joglo-interior",
-      "src": "/assets/img/gallery/sari/02.joglo-interior.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '02.joglo-interior',
+      'src': '/assets/img/gallery/sari/02.joglo-interior.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "03.joglo-lounge",
-      "src": "/assets/img/gallery/sari/03.joglo-lounge.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '03.joglo-lounge',
+      'src': '/assets/img/gallery/sari/03.joglo-lounge.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "04.joglo-kitchen",
-      "src": "/assets/img/gallery/sari/04.joglo-kitchen.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '04.joglo-kitchen',
+      'src': '/assets/img/gallery/sari/04.joglo-kitchen.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "05.joglo-kitchen-interior",
-      "src": "/assets/img/gallery/sari/05.joglo-kitchen-interior.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '05.joglo-kitchen-interior',
+      'src': '/assets/img/gallery/sari/05.joglo-kitchen-interior.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "06.joglo-bathroom",
-      "src": "/assets/img/gallery/sari/06.joglo-bathroom.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '06.joglo-bathroom',
+      'src': '/assets/img/gallery/sari/06.joglo-bathroom.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "07.joglo-patio-view",
-      "src": "/assets/img/gallery/sari/06.joglo-patio-view.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '07.joglo-patio-view',
+      'src': '/assets/img/gallery/sari/06.joglo-patio-view.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "08.apartment-interior",
-      "src": "/assets/img/gallery/sari/08.apartment-interior.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '08.apartment-interior',
+      'src': '/assets/img/gallery/sari/08.apartment-interior.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "09.apartment-interior-staircase",
-      "src": "/assets/img/gallery/sari/09.apartment-interior-staircase.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '09.apartment-interior-staircase',
+      'src': '/assets/img/gallery/sari/09.apartment-interior-staircase.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "10.apartment-bathroom",
-      "src": "/assets/img/gallery/sari/10.apartment-bathroom.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '10.apartment-bathroom',
+      'src': '/assets/img/gallery/sari/10.apartment-bathroom.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "11.common-area",
-      "src": "/assets/img/gallery/sari/11.common-area.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '11.common-area',
+      'src': '/assets/img/gallery/sari/11.common-area.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "12.joglo-patio-common-area",
-      "src": "/assets/img/gallery/sari/12.joglo-patio-common-area.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '12.joglo-patio-common-area',
+      'src': '/assets/img/gallery/sari/12.joglo-patio-common-area.jpg',
+      'w': 2560,
+      'h': 1706
     }
   ],
   boutique: [
     {
-      "pid": "01.pool-evening",
-      "src": "/assets/img/gallery/boutique/01.pool-evening.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '01.pool-evening',
+      'src': '/assets/img/gallery/boutique/01.pool-evening.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "02.modern-interior",
-      "src": "/assets/img/gallery/boutique/02.modern-interior.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '02.modern-interior',
+      'src': '/assets/img/gallery/boutique/02.modern-interior.jpg',
+      'w': 2560,
+      'h': 1706
     },
     {
-      "pid": "03.common-area",
-      "src": "/assets/img/gallery/boutique/03.common-area.jpg",
-      "w": 2560,
-      "h": 1706
+      'pid': '03.common-area',
+      'src': '/assets/img/gallery/boutique/03.common-area.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '04.joglo-front',
+      'src': '/assets/img/gallery/boutique/04.joglo-front.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '05.joglo-interior',
+      'src': '/assets/img/gallery/boutique/05.joglo-interior.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '06.joglo-lounge',
+      'src': '/assets/img/gallery/boutique/06.joglo-lounge.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '07.joglo-interior-kitchen',
+      'src': '/assets/img/gallery/boutique/07.joglo-interior-kitchen.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '08.joglo-bathroom',
+      'src': '/assets/img/gallery/boutique/08.joglo-bathroom.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '09.gladak-front',
+      'src': '/assets/img/gallery/boutique/09.gladak-front.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '10.gladak-south-interior',
+      'src': '/assets/img/gallery/boutique/10.gladak-south-interior.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '11.gladak-south-lounge',
+      'src': '/assets/img/gallery/boutique/11.gladak-south-lounge.jpg',
+      'w': 2560,
+      'h': 1706
+    },
+    {
+      'pid': '12.gladak-bathroom',
+      'src': '/assets/img/gallery/boutique/12.gladak-bathroom.jpg',
+      'w': 2560,
+      'h': 1706
     }
   ]
 }
