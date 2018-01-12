eeb882c60c91c9023c22a509aa9f036f1833d567
muji <muji@tmpfs.org>
2018-01-12 13:07:18 +0800

    Basic captions for gallery images.

--- src/assets/js/_gallery.js
+++ src/assets/js/_gallery.js
@@ -4,6 +4,7 @@ module.exports = {
       'pid': '01.joglo-front',
       'src': '/assets/img/gallery/sari/01.joglo-front.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/01.joglo-front.jpg',
+      'title': 'Joglo front view',
       'w': 1024,
       'h': 682
     },
@@ -11,6 +12,7 @@ module.exports = {
       'pid': '02.joglo-interior',
       'src': '/assets/img/gallery/sari/02.joglo-interior.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/02.joglo-interior.jpg',
+      'title': 'Joglo interior',
       'w': 1024,
       'h': 682
     },
@@ -18,6 +20,7 @@ module.exports = {
       'pid': '03.joglo-lounge',
       'src': '/assets/img/gallery/sari/03.joglo-lounge.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/03.joglo-lounge.jpg',
+      'title': 'Joglo lounge',
       'w': 1024,
       'h': 682
     },
@@ -25,6 +28,7 @@ module.exports = {
       'pid': '04.joglo-kitchen',
       'src': '/assets/img/gallery/sari/04.joglo-kitchen.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/04.joglo-kitchen.jpg',
+      'title': 'Joglo kitchen',
       'w': 1024,
       'h': 682
     },
@@ -32,6 +36,7 @@ module.exports = {
       'pid': '05.joglo-kitchen-interior',
       'src': '/assets/img/gallery/sari/05.joglo-kitchen-interior.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/05.joglo-kitchen-interior.jpg',
+      'title': 'Joglo kitchen interior',
       'w': 1024,
       'h': 682
     },
@@ -39,6 +44,7 @@ module.exports = {
       'pid': '06.joglo-bathroom',
       'src': '/assets/img/gallery/sari/06.joglo-bathroom.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/06.joglo-bathroom.jpg',
+      'title': 'Joglo bathroom',
       'w': 1024,
       'h': 682
     },
@@ -46,6 +52,7 @@ module.exports = {
       'pid': '07.joglo-patio-view',
       'src': '/assets/img/gallery/sari/07.joglo-patio-view.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/07.joglo-patio-view.jpg',
+      'title': 'Joglo patio view',
       'w': 1024,
       'h': 682
     },
@@ -53,6 +60,7 @@ module.exports = {
       'pid': '08.apartment-interior',
       'src': '/assets/img/gallery/sari/08.apartment-interior.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/08.apartment-interior.jpg',
+      'title': 'Apartment interior',
       'w': 1024,
       'h': 682
     },
@@ -60,6 +68,7 @@ module.exports = {
       'pid': '09.apartment-interior-staircase',
       'src': '/assets/img/gallery/sari/09.apartment-interior-staircase.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/09.apartment-interior-staircase.jpg',
+      'title': 'Apartment interior staircase',
       'w': 1024,
       'h': 682
     },
@@ -67,6 +76,7 @@ module.exports = {
       'pid': '10.apartment-bathroom',
       'src': '/assets/img/gallery/sari/10.apartment-bathroom.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/10.apartment-bathroom.jpg',
+      'title': 'Apartment bathroom',
       'w': 1024,
       'h': 682
     },
@@ -74,6 +84,7 @@ module.exports = {
       'pid': '11.common-area',
       'src': '/assets/img/gallery/sari/11.common-area.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/11.common-area.jpg',
+      'title': 'Common area',
       'w': 1024,
       'h': 682
     },
@@ -81,6 +92,7 @@ module.exports = {
       'pid': '12.joglo-patio-common-area',
       'src': '/assets/img/gallery/sari/12.joglo-patio-common-area.jpg',
       'msrc': '/assets/img/gallery/sari/thumbs/12.joglo-patio-common-area.jpg',
+      'title': 'Joglo patio common area',
       'w': 1024,
       'h': 682
     }
@@ -90,6 +102,7 @@ module.exports = {
       'pid': '01.pool-evening',
       'src': '/assets/img/gallery/boutique/01.pool-evening.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/01.pool-evening.jpg',
+      'title': 'Salt water swimming pool in the evening',
       'w': 1024,
       'h': 682
     },
@@ -97,6 +110,7 @@ module.exports = {
       'pid': '02.modern-interior',
       'src': '/assets/img/gallery/boutique/02.modern-interior.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/02.modern-interior.jpg',
+      'title': 'Modern interior',
       'w': 1024,
       'h': 682
     },
@@ -104,6 +118,7 @@ module.exports = {
       'pid': '03.common-area',
       'src': '/assets/img/gallery/boutique/03.common-area.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/03.common-area.jpg',
+      'title': 'Common area',
       'w': 1024,
       'h': 682
     },
@@ -111,6 +126,7 @@ module.exports = {
       'pid': '04.joglo-front',
       'src': '/assets/img/gallery/boutique/04.joglo-front.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/04.joglo-front.jpg',
+      'title': 'Joglo front',
       'w': 1024,
       'h': 682
     },
@@ -118,6 +134,7 @@ module.exports = {
       'pid': '05.joglo-interior',
       'src': '/assets/img/gallery/boutique/05.joglo-interior.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/05.joglo-interior.jpg',
+      'title': 'Joglo interior',
       'w': 1024,
       'h': 682
     },
@@ -125,6 +142,7 @@ module.exports = {
       'pid': '06.joglo-lounge',
       'src': '/assets/img/gallery/boutique/06.joglo-lounge.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/06.joglo-lounge.jpg',
+      'title': 'Joglo lounge',
       'w': 1024,
       'h': 682
     },
@@ -132,6 +150,7 @@ module.exports = {
       'pid': '07.joglo-interior-kitchen',
       'src': '/assets/img/gallery/boutique/07.joglo-interior-kitchen.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/07.joglo-interior-kitchen.jpg',
+      'title': 'Joglo interior kitchen',
       'w': 1024,
       'h': 682
     },
@@ -139,6 +158,7 @@ module.exports = {
       'pid': '08.joglo-bathroom',
       'src': '/assets/img/gallery/boutique/08.joglo-bathroom.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/08.joglo-bathroom.jpg',
+      'title': 'Joglo bathroom',
       'w': 1024,
       'h': 682
     },
@@ -146,6 +166,7 @@ module.exports = {
       'pid': '09.gladak-front',
       'src': '/assets/img/gallery/boutique/09.gladak-front.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/09.gladak-front.jpg',
+      'title': 'Gladak front view',
       'w': 1024,
       'h': 682
     },
@@ -153,6 +174,7 @@ module.exports = {
       'pid': '10.gladak-south-interior',
       'src': '/assets/img/gallery/boutique/10.gladak-south-interior.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/10.gladak-south-interior.jpg',
+      'title': 'Gladak south interior',
       'w': 1024,
       'h': 682
     },
@@ -160,6 +182,7 @@ module.exports = {
       'pid': '11.gladak-south-lounge',
       'src': '/assets/img/gallery/boutique/11.gladak-south-lounge.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/11.gladak-south-lounge.jpg',
+      'title': 'Gladak south lounge',
       'w': 1024,
       'h': 682
     },
@@ -167,6 +190,7 @@ module.exports = {
       'pid': '12.gladak-bathroom',
       'src': '/assets/img/gallery/boutique/12.gladak-bathroom.jpg',
       'msrc': '/assets/img/gallery/boutique/thumbs/12.gladak-bathroom.jpg',
+      'title': 'Gladak bathroom',
       'w': 1024,
       'h': 682
     }
