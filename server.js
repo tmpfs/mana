356fd8f74b27a1f47ab13c7876390b177ea7d07f
muji <muji@tmpfs.org>
2018-01-13 12:22:23 +0800

    Basic HTTP server.

---
+++ server.js
@@ -0,0 +1,60 @@
+const http = require('http')
+const server = http.createServer(handler)
+const qs = require('querystring')
+const ENDPOINT = '/contact'
+const MIME = 'application/json'
+
+function handler (req, res) {
+
+  function reply (status, message) {
+    res.statusCode = status
+    res.statusMessage = message
+    const pkt = {code: status, message: message}
+    const payload = JSON.stringify(pkt)
+    const len = Buffer.byteLength(payload)
+    res.setHeader('Content-Type', MIME)
+    res.setHeader('Content-Length', len)
+    res.write(payload)
+  }
+
+  const method = req.method
+  const url = req.url
+  if (method !== 'POST') {
+    return reply(405, 'Method not allowed')
+  }
+
+  if (url === ENDPOINT) {
+    let body = new Buffer(0)
+    req.on('data', function (data) {
+      body = Buffer.concat([body, data]);
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) {
+        return reply (413, 'Request entity too large')
+      }
+    })
+
+    req.on('end', function () {
+      const post = qs.parse(body.toString())
+      console.dir(post)
+    })
+
+    // Do not fall through to 404 catch all
+    return
+  }
+
+  return reply(404, 'Not found')
+}
+
+const listen = (port) => {
+  port = port || process.env.PORT || 3001
+  server.listen(port, () => {
+    console.log('listening on port %s', port)
+  })
+}
+
+if (!module.parent) {
+  listen()
+}
+
+module.exports = listen
