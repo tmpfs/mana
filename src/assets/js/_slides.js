889183fb4848ff1a966330220563bf4089b9ecef
muji <muji@tmpfs.org>
2018-01-14 12:09:24 +0800

    Reduce number of slideshow images.

--- src/assets/js/_slides.js
+++ src/assets/js/_slides.js
@@ -5,24 +5,12 @@ module.exports = {
     '03.joglo-lounge.jpg',
     '04.joglo-kitchen.jpg',
     '05.joglo-kitchen-interior.jpg',
-    '06.joglo-bathroom.jpg',
-    '07.joglo-patio-view.jpg',
-    '08.apartment-interior.jpg',
-    '09.apartment-interior-staircase.jpg',
-    '10.apartment-bathroom.jpg',
-    '11.common-area.jpg',
-    '12.joglo-patio-common-area.jpg'],
+    '06.joglo-bathroom.jpg'],
   boutique: [
     '01.pool-evening.jpg',
     '02.modern-interior.jpg',
     '03.common-area.jpg',
     '04.joglo-front.jpg',
     '05.joglo-interior.jpg',
-    '06.joglo-lounge.jpg',
-    '07.joglo-interior-kitchen.jpg',
-    '08.joglo-bathroom.jpg',
-    '09.gladak-front.jpg',
-    '10.gladak-south-interior.jpg',
-    '11.gladak-south-lounge.jpg',
-    '12.gladak-bathroom.jpg']
+    '06.joglo-lounge.jpg']
 }
